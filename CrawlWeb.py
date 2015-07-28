e0b6773a352c42469f6b7b31a375d3aa9cc6406c
Nicholas Corneau <nicholascorneau@gmail.com>
2015-07-28 12:32:12 -0400

    Update CrawlWeb.py
    #CrawlWeb(seed)
    
    1 in, 1 out
    
    Given a seed page, retrieve all the out-links until a dead-end or loop is reached. Return a list of pages crawled.

--- CrawlWeb.py
+++ CrawlWeb.py
@@ -1,9 +1,11 @@
 def crawl_web(seed):
     tocrawl = [seed]
     crawled = []
+    
     while tocrawl:
         page = tocrawl.pop()
         if page not in crawled:
             union(tocrawl, get_all_links(get_page(page)))
             crawled.append(page)
+            
     return crawled

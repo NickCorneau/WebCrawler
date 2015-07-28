efea2eb10a28640028ab6b8c017a69ae1c64e856
Nick <nicholascorneau@gmail.com>
2015-07-28 10:36:49 -0400

    Function to crawl a webpage and return all its links

---
+++ GetAllLinks.py
@@ -0,0 +1,10 @@
+def get_all_links(page):
+	links = []
+	while True:
+		url, endpos = get_next_target(page)
+		if url:
+			links.append(url)
+			page = page[endpos:]
+		else:
+			break
+	return links

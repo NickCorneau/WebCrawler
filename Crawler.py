2486fafda3dd2bbc3489b053ad310dc1d4f6e340
Nick <nicholascorneau@gmail.com>
2015-08-12 12:11:58 -0400

    Added a webpage ranker function and updated crawl_web to return a friendship graph

--- Crawler.py
+++ Crawler.py
@@ -1,35 +1,132 @@
 """ HACKISH FUNCTION TO FAKE A geturl """
+cache = {
+   'http://udacity.com/cs101x/urank/index.html': """<html>
+<body>
+<h1>Dave's Cooking Algorithms</h1>
+<p>
+Here are my favorite recipies:
+<ul>
+<li> <a href="http://udacity.com/cs101x/urank/hummus.html">Hummus Recipe</a>
+<li> <a href="http://udacity.com/cs101x/urank/arsenic.html">World's Best Hummus</a>
+<li> <a href="http://udacity.com/cs101x/urank/kathleen.html">Kathleen's Hummus Recipe</a>
+</ul>
+
+For more expert opinions, check out the 
+<a href="http://udacity.com/cs101x/urank/nickel.html">Nickel Chef</a> 
+and <a href="http://udacity.com/cs101x/urank/zinc.html">Zinc Chef</a>.
+</body>
+</html>
+
+
+
+
+
+
+""", 
+   'http://udacity.com/cs101x/urank/zinc.html': """<html>
+<body>
+<h1>The Zinc Chef</h1>
+<p>
+I learned everything I know from 
+<a href="http://udacity.com/cs101x/urank/nickel.html">the Nickel Chef</a>.
+</p>
+<p>
+For great hummus, try 
+<a href="http://udacity.com/cs101x/urank/arsenic.html">this recipe</a>.
+
+</body>
+</html>
+
+
+
+
+
+
+""", 
+   'http://udacity.com/cs101x/urank/nickel.html': """<html>
+<body>
+<h1>The Nickel Chef</h1>
+<p>
+This is the
+<a href="http://udacity.com/cs101x/urank/kathleen.html">
+best Hummus recipe!
+</a>
+
+</body>
+</html>
+
+
+
+
+
+
+""", 
+   'http://udacity.com/cs101x/urank/kathleen.html': """<html>
+<body>
+<h1>
+Kathleen's Hummus Recipe
+</h1>
+<p>
+
+<ol>
+<li> Open a can of garbonzo beans.
+<li> Crush them in a blender.
+<li> Add 3 tablesppons of tahini sauce.
+<li> Squeeze in one lemon.
+<li> Add salt, pepper, and buttercream frosting to taste.
+</ol>
+
+</body>
+</html>
+
+""", 
+   'http://udacity.com/cs101x/urank/arsenic.html': """<html>
+<body>
+<h1>
+The Arsenic Chef's World Famous Hummus Recipe
+</h1>
+<p>
+
+<ol>
+<li> Kidnap the <a href="http://udacity.com/cs101x/urank/nickel.html">Nickel Chef</a>.
+<li> Force her to make hummus for you.
+</ol>
+
+</body>
+</html>
+
+""", 
+   'http://udacity.com/cs101x/urank/hummus.html': """<html>
+<body>
+<h1>
+Hummus Recipe
+</h1>
+<p>
+
+<ol>
+<li> Go to the store and buy a container of hummus.
+<li> Open it.
+</ol>
+
+</body>
+</html>
+
+
+
+
+""", 
+}
+
 def get_page(url):
-    try:
-        if url == "http://www.udacity.com/cs101x/index.html":
-            return '''<html> <body> This is a test page for learning to crawl!
-<p> It is a good idea to
-<a href="http://www.udacity.com/cs101x/crawling.html">
-learn to crawl</a> before you try to
-<a href="http://www.udacity.com/cs101x/walking.html">walk</a> or
-<a href="http://www.udacity.com/cs101x/flying.html">fly</a>.</p></body>
-</html>'''
-
-        elif url == "http://www.udacity.com/cs101x/crawling.html":
-            return '''<html> <body> I have not learned to crawl yet, but I am
-quite good at  <a href="http://www.udacity.com/cs101x/kicking.html">kicking</a>.
-</body> </html>'''
-
-        elif url == "http://www.udacity.com/cs101x/walking.html":
-            return '''<html> <body> I cant get enough
-<a href="http://www.udacity.com/cs101x/index.html">crawling</a></body></html>'''
-
-        elif url == "http://www.udacity.com/cs101x/flying.html":
-            return '''<html>
-<body>The magic words are Squeamish Ossifrage!</body></html>'''
-    except:
-        return ""
-    return ""
+    if url in cache:
+        return cache[url]
+    else:
+        return None
 
 """ Gets all links based on <a href= """
 def get_next_target(page):
     start_link = page.find('<a href=')
-    if start_link == -1:
+    if start_link == -1: 
         return None, 0
     start_quote = page.find('"', start_link)
     end_quote = page.find('"', start_quote + 1)
@@ -47,19 +144,46 @@ def get_all_links(page):
             break
     return links
 
-def crawl_web(seed):
+def crawl_web(seed): # returns index, graph of inlinks
     tocrawl = [seed]
     crawled = []
-    index = {}
-    while tocrawl:
+    graph = {}  # <url>, [list of pages it links to]
+    index = {} 
+    while tocrawl: 
         page = tocrawl.pop()
         if page not in crawled:
-            content = get_page(page) # STILL FAKE ONLY WORKS FOR 3 URL's 
+            content = get_page(page)
             add_page_to_index(index, page, content)
-            union(tocrawl, get_all_links(content))
+            outlinks = get_all_links(content)
+            
+            
+            graph[page] = outlinks
+            
+            
+            union(tocrawl, outlinks)
             crawled.append(page)
-    return index
+    return index, graph
 
+def compute_ranks(graph):
+    d = 0.8 # damping factor
+    numloops = 10
+    
+    ranks = {}
+    npages = len(graph)
+    for page in graph:
+        ranks[page] = 1.0 / npages
+    
+    for i in range(0, numloops):
+        newranks = {}
+        for page in graph:
+            newrank = (1 - d) / npages
+            for node in graph:
+                if page in graph[node]:
+                    newrank = newrank + d * (ranks[node] / len(graph[node]))
+            newranks[page] = newrank
+        ranks = newranks
+    return ranks
+	
 def add_page_to_index(index, url, content):
     words = content.split()
     for word in words:
@@ -82,5 +206,9 @@ def union(a, b):
             a.append(e)
 
 """ TESTING """
-index = (crawl_web("http://www.udacity.com/cs101x/index.html"))
-print(lookup(index, "idea"))
+index, graph = crawl_web('http://udacity.com/cs101x/urank/index.html')
+ranks = compute_ranks(graph)
+print(lookup(index, "Nickel"))
+ranks = compute_ranks(graph)
+print ranks
+
